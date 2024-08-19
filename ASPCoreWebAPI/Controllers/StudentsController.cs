0cea8f799d18186853a7c13f98b6b15a68211503
Prasanta Biswal <pkbstudy@gmail.com>
2024-08-19 11:48:44 +0530

    Add project files.

---
+++ ASPCoreWebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        public List<string> student = new List<string>()
+        {
+            "prasanta",
+            "Leena",
+            "raj"
+        };
+        [HttpGet] 
+        public List<string> getStudent() 
+        {
+            return student;
+        }
+        [HttpGet("{id}")]
+        public string getStudentByID(int id)
+        {
+            return student.ElementAt(id);
+        }
+    }
+}
